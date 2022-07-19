d1697d7778f38edc71baaec4d522fea43f2a6c5f
mariananizetich <mariananizetich@gmail.com>
2022-07-19 13:58:43 -0300

    funciones

--- JS/ciclos.js
+++ JS/ciclos.js
@@ -1,39 +1,50 @@
-let excursion1 = "Avistaje de ballenas"
+let excursion1 = "Avistaje"
 let precio1 = 10500
-let stock1 = 10
 
-let excursion2 = "Snorkel con lobos marinos"
+let excursion2 = "Snorkel"
 let precio2 = 9000
-let stock2 = 10
+
+let stock = 10
 
 
 let cantidadTotal = prompt ("Cuántas excursiones desea comprar?")
 let precioFinal = 0
+if (cantidadTotal )
+
+function sumaPrecio(cantidad, precio){
+    precioFinal += cantidad * precio
+}
+
+function calculoStock(cantidad, stock, precio){
+    if(stock >= cantidad){
+        sumaPrecio(cantidad, precio)
+        alert("El precio total es de: $" + (cantidad * precio))
+    }
+    else{
+        alert("Error: Podés comprar hasta " + stock + " tickets por persona")
+        
+    }
+}
+
 
 for (let i = 0; i < cantidadTotal; i++) {
 
-let compra1 = prompt ("Ingrese la excursión que quiere comprar: \n- Avistaje de ballenas \n- Snorkel con lobos marinos")
+let compra1 = prompt ("Ingrese la excursión que quiere comprar: \n- Avistaje \n- Snorkel")
 let cantidad1 = prompt ("Ingrese la cantidad de tickets que desea comprar:")
-
-if (compra1 == "Avistaje de ballenas") {
-    if (stock1 >= cantidad1){
-        precioFinal += cantidad1 * precio1
-        alert ("El precio total es de: $" + (cantidad1 * precio1))
-    }
-    else {
-        alert ("Error: Podés comprar hasta 10 tickets por persona.")
-    }
+if (cantidad1 >stock) {
+    alert ("Error: Podés comprar hasta " + stock + " tickets por persona");
 }
 
-else if (compra1 == "Snorkel con lobos marinos") {
-    if (stock2 >= cantidad1){
-        precioFinal += cantidad1 * precio2
-        alert ("El precio total es de: $" + (cantidad1 * precio2))
-    }
-    else {
-        alert ("Error: Podés comprar hasta 10 tickets por persona.")
-    }
+if (compra1 == "Avistaje") {
+    calculoStock (cantidad1, stock, precio1)
+}
+
+else if (compra1 == "Snorkel") {
+    calculoStock (cantidad1, stock, precio2)
 }
 
 }
+
+
 alert ("Listo! El precio total de tu compra es de: $" + precioFinal + ". Que lo disfrutes!")
+
